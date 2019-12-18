71e7ce352353304614b41b950238a4feed9cae5b
hiroyukiusui <58940399+hiroyukiusui@users.noreply.github.com>
2019-12-18 20:55:50 +0900

    usuitest001

---
+++ TimerApp/TimerApp/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  TimerApp
+//
+//  Created by Yosuke Nozaki on 2019/12/06.
+//  Copyright © 2019 Yosuke Nozaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var timer: Timer?
+    var duration = 0
+    let settingKey = "timerValue"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let settinngs = UserDefaults.standard
+        settinngs.register(defaults: [settingKey:60])
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        duration = 0
+        _ = displayUpdate()
+    }
+    
+    @IBOutlet weak var timeDisplay: UILabel!
+    
+    @IBAction func settingButtonAction(_ sender: Any) {
+        if let nowTimer = timer {
+            if nowTimer.isValid == true {
+                nowTimer.invalidate()
+            }
+        }
+        performSegue(withIdentifier: "openSetting", sender: nil)
+    }
+    
+    @IBAction func startTimerAction(_ sender: Any) {
+        if let nowTimer = timer {
+            if nowTimer.isValid == true {
+                return
+            }
+        }
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.timerStop(_:)), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func stopTimerAction(_ sender: Any) {
+        if let nowtimer = timer {
+            if nowtimer.isValid == true {
+                nowtimer.invalidate()
+            }
+        }
+    }
+    
+    func displayUpdate() -> Int {
+        let settings = UserDefaults.standard
+        let timeValue = settings.integer(forKey: settingKey)
+        let remainSeconds = timeValue - duration
+        timeDisplay.text = "あと\(remainSeconds)秒"
+        return remainSeconds
+    }
+    
+    @objc func timerStop(_ timer:Timer){
+        duration += 1
+        if displayUpdate() <= 0 {
+            duration = 0
+            timer.invalidate()
+        }
+    }
+}
+
